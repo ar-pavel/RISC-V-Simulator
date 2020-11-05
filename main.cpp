5e5e12da3ce30b7c3f146a31efb61ab04beb2c11
Tanvir Sojal <tanvirahmedsojal@gmail.com>
2020-11-05 19:36:13 +0600

    options handling code added

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
-
-#include "hex.h"
-
-#include "memory.h"
-
-#include "rv32i.h"
-
 #include <stdlib.h>
-
 #include <iostream>
-
 #include <unistd.h>
 
-using namespace std;
+#include "hex.h"
+#include "memory.h"
+#include "rv32i.h"
 
 /**
  * Print a usage message and abort the program.
  *********************************************************************/
 static void usage()
 {
-    cerr << "Usage: rv32i [-m hex-mem-size] infile" << endl;
-    cerr << "    -m specify memory size (default = 0x10000)" << endl;
+    std::cerr << "Usage: rv32i [-m hex-mem-size] infile" << std::endl;
+    std::cerr << "    -m specify memory size (default = 0x10000)" << std::endl;
     exit(1);
 }
 
@@ -29,32 +22,78 @@ static void usage()
 int main(int argc, char **argv)
 {
     uint32_t memory_limit = 0x10000; // default memory size = 64k
+    uint64_t execution_limit = 0;    // 0 = run forever
     int opt;
 
-    while ((opt = getopt(argc, argv, "m:")) != -1)
+    bool show_disasm = false;
+    bool show_insn = false;
+    bool show_regs = false;
+    bool show_mem_dump = false;
+
+    while ((opt = getopt(argc, argv, "dil::m::rz")) != -1)
     {
         switch (opt)
         {
-        case 'm':
-            memory_limit = std::stoul(argv[2], nullptr, 16);
+        case 'd':
+            show_disasm = true;
+            // std::cout << " found d at \n";
             break;
+        case 'i':
+            // std::cout << " found i at \n";
+            show_insn = true;
+            break;
+        case 'l':
+            // std::cout << " found l at \n";
+            execution_limit = (uint32_t)std::stoul(optarg, nullptr, 10);
+            // std::cout << " execution limit: " << execution_limit << "\n";
+            break;
+        case 'm':
+            // hex_mem_size given as argument is assigned to memory limit
+            memory_limit = (uint32_t)std::stoul(optarg, nullptr, 16);
+            // std::cout << " found m at \n";
+            // std::cout << " memory limit: " << memory_limit << "\n";
+            break;
+        case 'r':
+            // std::cout << " found r at \n";
+            show_regs = true;
+            break;
+        case 'z':
+            // std::cout << " found z at \n";
+            show_mem_dump = true;
+            break;
+
         default:
             /* '?' */
             usage();
         }
     }
-
-    if (3 >= argc)
-        usage(); // missing filename
+    // std::cout << " reading instructions from: " << argv[argc - 1] << std::endl;
+    // if (3 >= argc)
+    //     usage(); // missing filename
 
     memory mem(memory_limit);
 
-    if (!mem.load_file(argv[3]))
+    // missing filename or file loading error
+    if (!mem.load_file(argv[argc - 1]))
         usage();
 
     rv32i sim(&mem);
-    sim.disasm();
-    mem.dump();
 
+    // print instructions if option -i is given
+    sim.set_show_instructions(show_insn);
+
+    // print register hart dump if option -r is given
+    sim.set_show_registers(show_regs);
+
+    // run the simulated with fixed limit if -l flag has an argument
+    sim.run(execution_limit);
+
+    // disassemble the instructions
+    if (show_disasm)
+        sim.disasm();
+
+    // show memory dump
+    if (show_mem_dump)
+        mem.dump();
     return 0;
 }
